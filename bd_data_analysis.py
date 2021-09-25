c619446278449bb2096c620954daa9bc6037f190
Nachiket Gokhale <gokhalen@gmail.com>
2021-09-25 17:20:16 +0530

    Changes to be committed: 	modified:   .gitignore 	new file:   bd_data_analysis.py 	modified:   scraper.py

---
+++ bd_data_analysis.py
@@ -0,0 +1,83 @@
+import pandas as pd
+import json
+import re
+import sys
+# config
+jsonfile  = 'scrape/data.json'
+jsonclean = 'scrape/data.json.clean'
+processed = []
+
+
+# functions and classes
+
+
+
+def remove_punc(lyrics_str):
+    '''
+    removes '?',',' and other punc from text 
+    '''
+    puncs = ['?',',',')','(']
+    out   = lyrics_str
+    for punc in puncs:
+        out = out.replace(punc,'')
+
+    return out
+
+def remove_copyright(lyrics_str,songname):
+    '''
+    removes copyright message from the lyrics
+    this is an interactive function
+    '''
+
+    songlength = len(lyrics_str)
+    out = lyrics_str
+
+    # https://stackoverflow.com/questions/3519565/find-the-indexes-of-all-regex-matches    
+    copyiter  = re.finditer('copyright',lyrics_str,flags=re.IGNORECASE)
+    copystart = [ m.start(0) for m in copyiter ]
+
+    if len(copystart) not in [0,1]:
+        print(f'multiple copyrights detected for {songname=}')
+
+    start = copystart[0]
+    copyrightratio = start/songlength
+
+    print(f'{copyrightratio=}')
+    
+    return out
+
+def check_word(lyrics_str,songname,wordlist):
+    for word in wordlist:
+        worditer = re.finditer(word,lyrics_str,flags=re.IGNORECASE)
+        wordstart = [ m.start(0) for m in worditer ]
+        if len(wordstart) != 0:
+            print(f'{word} detected in {songname}')
+
+            # breakpoint()
+        
+
+def processing_message(songname):
+    print('-'*80)
+    print(f'Processing {songname}')
+    print('-'*80)
+
+# main program
+with open(jsonfile,'rt') as fin:
+    bd_datalist = json.load(fin)
+
+for bd_dict in bd_datalist:
+    songlink   = bd_dict['songlink']
+    songname   = bd_dict['songname']
+    songlyrics = bd_dict['songlyrics']
+    
+    # processing_message(songname)
+    new_lyrics = remove_punc(songlyrics)
+    new_lyrics = remove_copyright(new_lyrics,songname)
+    check_word(new_lyrics,songname,['alternate version'])
+
+    bd_dict['cleanlyrics'] = new_lyrics
+    
+    processed.append(bd_dict)
+    
+with open(jsonclean,'wt') as fout:
+    json.dump(processed,fout,indent=4)
--- scraper.py
+++ scraper.py
@@ -18,12 +18,11 @@ basejson  = f'{basepath}/data.json'
 
 songlist  = []
 
-# if songs.html does not exist, scrape it and save it
-if not os.path.isfile(basefile):
-    print(f'Loading {baseurl}')
-    resp    = requests.get(baseurl)
-    with open(basefile,'wt') as fout:
-        fout.write(resp.text)
+# scrape base
+print(f'Loading {baseurl}')
+resp    = requests.get(baseurl)
+with open(basefile,'wt') as fout:
+    fout.write(resp.text)
 
 # read songs.html            
 from bs4 import BeautifulSoup
@@ -37,9 +36,11 @@ for l in ll:
 soup  = BeautifulSoup(htmlstr,'html.parser')
 # https://stackoverflow.com/questions/16248723/how-to-find-spans-with-a-specific-class-containing-specific-text-using-beautiful
 spans     = soup.find_all('span',{'class':'song'})
-songnames = [span.get_text() for span in spans]
-songlinks = []
-filenames = []
+
+# song properties
+songnames  = [span.get_text() for span in spans]
+songlinks  = []
+songlyrics = []
 
 # make links 
 for span in spans:
@@ -47,33 +48,31 @@ for span in spans:
     for link in links:
         songlinks.append(str(link['href']))
         
-# make filenames
-for name in songnames:
-    filenames.append(sanitize_filename(name))
-
-# check to see that there are not duplicates     
-assert (len(songlinks)==len(songnames) == len(filenames) == len(set(songlinks)) == len(set(songnames)) == len(set(filenames)) \
-),'length of songlinks,songnames,filenames is not same'
-
 number_of_songs = len(songlinks)
-
 print(f'{number_of_songs=}')
 
-# breakpoint()
-# save song info
-dd = {'songlinks':songlinks,'songnames':songnames,'filenames':filenames}
-with open(basejson,'wt') as fout:
-    json.dump(dd,fout)
-
-
-link  = songlinks[0]
-fname = basepath+'/'+filenames[0]
-if not os.path.isfile(fname):
+for ictr,link in enumerate(songlinks):
+    #link  = songlinks[2]
+    print(f'Retrieving and parsing {link=} {ictr} of {number_of_songs}')
     resp = requests.get(link)
     soup = BeautifulSoup(resp.text,'html.parser')
     lyrics = soup.find('div',{'class':'article-content lyrics'}).get_text()
-    copylist = re.findall('copyright',lyrics,flags=re.IGNORECASE)
-    print(lyrics)
-    breakpoint()
-    time.sleep(0.05)
+    songlyrics.append(lyrics)
+    # copylist = re.findall('copyright',lyrics,flags=re.IGNORECASE)
+    # breakpoint()
+    time.sleep(0.2)
 
+
+# check to see that there are not duplicates     
+assert (len(songlinks)==len(songnames) ==  \
+        len(set(songnames)) == len(set(songlinks)) \
+       ),'length of songlinks,songnames, is not same'
+    
+outlist = []
+
+for _link, _name, _lyrics in zip(songlinks,songnames,songlyrics):
+    dd = {'songlink':_link, 'songname':_name,'songlyrics':_lyrics}
+    outlist.append(dd)
+
+with open(basejson,'wt') as fout:
+    json.dump(outlist,fout,indent=4)
